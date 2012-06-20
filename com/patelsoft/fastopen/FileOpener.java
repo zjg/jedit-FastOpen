99348f41c3c18f1370f730ce7a3aeaeaea81cc8d
ezust <ezust@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2012-06-20 16:35:38 +0000

    Updated docs.
    git-svn-id: https://jedit.svn.sourceforge.net/svnroot/jedit/plugins/FastOpen/trunk@21842 6b1eeb88-9816-0410-afa2-b43733a0f04e

--- com/patelsoft/fastopen/FileOpener.java
+++ com/patelsoft/fastopen/FileOpener.java
@@ -1,8 +1,9 @@
 package com.patelsoft.fastopen;
 
 import org.gjt.sp.jedit.View;
-/**
+/** Used by ErrorList 2.0 to open files it has a name but no path for.
   @author Alan Ezust
+  @since jEdit 5.0pre1
  */
 
 public class FileOpener extends org.jedit.core.FileOpenerService {
